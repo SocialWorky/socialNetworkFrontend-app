c8c0f4567281e3ca49bc2dda324a6347fc7b7aa5
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-13 00:33:38 -0400

    Update title in index.html and initialize translations in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { Translations } from './translations/translations';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+const translationsInitializer = new Translations();
+
+async function initializeApp() {
+  try {
+    await translationsInitializer.initialize();
+    platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+    defineCustomElements(window);
+  } catch (error) {
+    console.error('Error initializing translations:', error);
+  }
+}
+
+initializeApp();
