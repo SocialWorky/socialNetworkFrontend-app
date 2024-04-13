7c7087f2990e73bed96aeb2ea86b4893bceb27b4
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-12 22:09:45 -0400

    Update app.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: [],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) {
+    document.body.classList.add('light-theme');
+    //this.renderer.setAttribute(this.document.documentElement, 'lang', getTranslationsLanguage());
+  }
 }
