91dac892dd894fb884972ee62eee7775b1308b08
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-15 13:52:02 -0400

    Updating the login component

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { WorkyButtonType, WorkyButtonTheme } from '../../../shared/buttons/models/worky-button-model';
 
 @Component({
   selector: 'worky-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent { }
+export class LoginComponent { 
+
+  WorkyButtonType = WorkyButtonType;
+
+  WorkyButtonTheme = WorkyButtonTheme;
+
+  constructor() {}
+
+  login() {
+    console.log('Login');
+  }
+
+}
