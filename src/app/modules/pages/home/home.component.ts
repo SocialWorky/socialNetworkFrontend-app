6bb3a6a8b576e78303c1af2dfd3229e19dbcc8ff
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-16 14:53:12 -0400

    Add home component

---
+++ src/app/modules/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'worky-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+
+  constructor() { }
+
+}
