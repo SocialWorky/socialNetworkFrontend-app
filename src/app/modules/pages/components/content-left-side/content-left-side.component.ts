a507a757f50cb6d452b240110ad14c36146f2cd3
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-16 14:53:01 -0400

    Add content-left-side and content-right-side components

---
+++ src/app/modules/pages/components/content-left-side/content-left-side.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'worky-content-left-side',
+  templateUrl: './content-left-side.component.html',
+  styleUrls: ['./content-left-side.component.scss'],
+})
+export class ContentLeftSideComponent  {
+
+  constructor() { }
+
+
+}
