2d95d2c1ccd5d849276d5a3cbf88ab06fb5b594a
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-16 20:50:59 -0400

    Update login component with translated error messages

--- src/translations/translations.es.ts
+++ src/translations/translations.es.ts
@@ -5,10 +5,16 @@ export const translations = {
   'login.signInWithGoogle': 'Iniciar sesión con Google',
   'login.orLoginWithEmail': 'O inicia sesión con tu email',
   'login.forgotPassword': '¿Has olvidado la contraseña?',
+  'login.emailOrPasswordIncorrect': 'El correo electrónico o la contraseña son incorrectos',
+  'login.messageErrorHeader': 'Error al iniciar sesión',
+  'login.messageLoading': 'Conectando...',
+  'login.messageErrorCredentials': 'Credenciales incorrectas. Por favor, inténtalo de nuevo.',
+  'login.messageErrorServer': 'Error en el servidor. Por favor, inténtalo de nuevo.',
   // INPUTS
   'input.email': 'Correo electrónico',
   'input.password': 'Contraseña',
   // BUTTONS
   'button.login': 'Iniciar sesión',
   'button.signUp': 'Registrarse',
+  'button.ok': 'Aceptar',
 };
