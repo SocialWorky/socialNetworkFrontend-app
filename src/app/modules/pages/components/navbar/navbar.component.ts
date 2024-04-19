922b9ff604111064517a94d57042ce5027a33ae3
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-19 12:16:41 -0400

    Update layout component to include navbar and content sections

--- src/app/modules/pages/components/navbar/navbar.component.ts
+++ src/app/modules/pages/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'worky-navbar',
@@ -6,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  token = localStorage.getItem('token');
 
-  constructor() { }
+  searchTerm: string = '';
+
+  constructor(private _router: Router) { }
+
+  logoutUser() {
+    localStorage.removeItem('token');
+    this._router.navigate(['/auth']);
+  }
+
+  search(event: Event) {
+    // Realiza la búsqueda si el término tiene al menos 3 caracteres
+    if (this.searchTerm.trim().length >= 3) {
+      console.log('Buscar:', this.searchTerm);
+    }
+  }
+
+  onInputChange(event: Event) {
+    // Realiza la búsqueda automáticamente si se escriben más de 3 caracteres
+    if (this.searchTerm.trim().length >= 3) {
+      this.search(event);
+    }
+  }
+
+  onEnter(event: any) {
+    // Realiza la búsqueda si se presiona "Enter" y el término tiene al menos 3 caracteres
+    if (event.key === 'Enter' && this.searchTerm.trim().length >= 3) {
+      this.search(event);
+    }
+  }
 
 }
