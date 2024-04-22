2a425db576076171deeafcdee93d6506e885c1ac
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-21 21:36:23 -0400

    Add profiles component HTML and TypeScript files

---
+++ src/app/modules/pages/profiles/profiles.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profiles',
+  templateUrl: './profiles.component.html',
+  styleUrls: ['./profiles.component.scss'],
+})
+export class ProfilesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
