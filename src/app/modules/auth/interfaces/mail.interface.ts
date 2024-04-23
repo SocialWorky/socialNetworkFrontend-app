a02f2650f60b5fa95acbe8a34484bf005984e14d
Carlos Abarca Vega <soporte@compuelec.cl>
2024-04-23 12:06:49 -0400

    Add MailRegisterValidateData interface for email registration validation

---
+++ src/app/modules/auth/interfaces/mail.interface.ts
@@ -0,0 +1,9 @@
+export interface MailRegisterValidateData {
+  url: string;
+  subject: string;
+  title: string;
+  greet: string;
+  message: string;
+  subMessage: string;
+  buttonMessage: string;
+}
