d38ba03fb0761186e25a01af68efccb2e1d150d5
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-07 15:05:17 -0400

    chore: Update BASE_URL in environment.prod.ts to use production URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   PRODUCTION: true,
   GIT_SHA: 'GIT_SHA',
   API_URL: 'https://backend-dev.worky.cl/api/v1',
-  BASE_URL: 'http://localhost:4200',
+  BASE_URL: 'https://dev.worky.cl',
   CLIEN_ID_GOOGLE: '392454930418-dunulmihmuss90c042vn359mitdjjko8.apps.googleusercontent.com',
   APP_PORT: 4200,
   APP_VERSION: '1.0.0',
