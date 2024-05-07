e15d6032215dea43b8e3c568bfb090673e3096e7
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-07 12:34:33 -0400

    chore: Update environment variables to use process.env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
-import { env } from '../../env'
-
 export const environment = {
   PRODUCTION: true,
-  API_URL: env.API_URL,
-  BASE_URL: env.BASE_URL,
-  CLIEN_ID_GOOGLE: env.CLIEN_ID_GOOGLE,
+  API_URL: process.env.API_URL,
+  BASE_URL: process.env.BASE_URL,
+  CLIEN_ID_GOOGLE: process.env.CLIEN_ID_GOOGLE,
 };
