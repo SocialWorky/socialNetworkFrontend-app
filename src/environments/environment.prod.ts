0daab7d814ac611f8eeae1c989a1c0dc7b6a445f
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-07 12:56:28 -0400

    chore: Update environment variables to use 'env' package and set default values in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
-const env = require('../../env')
+if (require('../../env')) {
+  const env = require('../../env')
+}
 
 export const environment = {
   PRODUCTION: true,
-  API_URL: env.API_URL,
-  BASE_URL: env.BASE_URL,
-  CLIEN_ID_GOOGLE: env.CLIEN_ID_GOOGLE,
-  APP_PORT: env.APP_PORT,
-  APP_VERSION: env.APP_VERSION,
+  API_URL: env.API_URL || 'http://localhost/api/v1',
+  BASE_URL: env.BASE_URL || 'http://localhost:4200',
+  CLIEN_ID_GOOGLE: env.CLIEN_ID_GOOGLE || 'apps.googleusercontent.com',
+  APP_PORT: env.APP_PORT || 4200,
+  APP_VERSION: env.APP_VERSION || '1.0.0',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,17 @@
-const env = require('../../env')
-
+if (require('../../env')) {
+  const env = require('../../env')
+}
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   PRODUCTION: false,
-  API_URL: env.API_URL,
-  BASE_URL: env.BASE_URL,
-  CLIEN_ID_GOOGLE: env.CLIEN_ID_GOOGLE,
-  APP_PORT: env.APP_PORT,
-  APP_VERSION: env.APP_VERSION,
+  API_URL: env.API_URL || 'http://localhost/api/v1',
+  BASE_URL: env.BASE_URL || 'http://localhost:4200',
+  CLIEN_ID_GOOGLE: env.CLIEN_ID_GOOGLE || 'apps.googleusercontent.com',
+  APP_PORT: env.APP_PORT || 4200,
+  APP_VERSION: env.APP_VERSION || '1.0.0',
 };
 
 /*
