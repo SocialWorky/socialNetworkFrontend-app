880ece100cd797cf4077c841b9e9ac5b61875859
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-09 12:06:58 -0400

    Update: Add token interface and update worky-dropdown component

--- src/app/modules/shared/worky-dropdown/worky-dropdown.component.ts
+++ src/app/modules/shared/worky-dropdown/worky-dropdown.component.ts
@@ -1,11 +1,18 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DropdownDataLink } from './interfaces/dataLink.interface';
+import { jwtDecode } from 'jwt-decode';
+import { Token } from '../interfaces/token.interface';
+
 @Component({
   selector: 'worky-dropdown',
   templateUrl: './worky-dropdown.component.html',
   styleUrls: ['./worky-dropdown.component.scss'],
 })
 export class WorkyDropdownComponent  implements OnInit {
+
+  userName: string = '';
+
+  token = localStorage.getItem('token');
 
   @Input() icon: string = 'add_circle';
 
@@ -21,8 +28,12 @@ export class WorkyDropdownComponent  implements OnInit {
 
   constructor() { }
 
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.token) {
+      const decodedToken: Token = jwtDecode(this.token);
+      this.userName = decodedToken.name;
+    }
+  }
 
   handleMenuItemClick(data: DropdownDataLink) {
     this.linkClicked.emit(data);
