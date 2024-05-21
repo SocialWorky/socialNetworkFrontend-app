8274d6bf480fe2216559b550d66b1057ced25ab4
OSCARS26 <oscarolivare1@gmail.com>
2024-05-20 20:20:10 -0400

    feat: Add WorkyNewsComponent for displaying news articles
    The WorkyNewsComponent is added to display news articles in a widget. It fetches top sports headlines using the NewsService and displays them in a responsive layout. The component template and styles are also added.

---
+++ src/app/modules/shared/worky-widget/worky-news/service/apiNews.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NewsResponse } from '../interface/dataNews.interface';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NewsService {
+    private apiUrl = 'https://newsapi.org/v2/top-headlines';
+    private apiKey = ''; // Reemplaza con tu API key de NewsAPI b622b6a2f70745199032c7267f98df6b
+
+    constructor(private http: HttpClient) {}
+
+    getTopSportsHeadlines(): Observable<NewsResponse> {
+        return this.http.get<NewsResponse>(`${this.apiUrl}?category=sports&apiKey=${this.apiKey}`);
+    }
+}
