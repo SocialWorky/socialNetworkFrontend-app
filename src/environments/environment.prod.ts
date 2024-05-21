95f8e112ca36387a25c1f1068ed1fe58a1554bb9
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-20 20:01:01 -0400

    Update APIGEOLOCATIONS URL in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   APP_VERSION: '1.0.0',
   WSURL: 'https://notifications-dev.worky.cl',
   OPENCAGEAPIKEY: 'a7f24f0e6069426b9480725b48bf0403',
-  APIGEOLOCATIONS: 'http://localhost:3013/api/v1', // GeoLocations API Service worky-geo-locations
+  APIGEOLOCATIONS: 'https://geo-dev.worky.cl/api/v1', // GeoLocations API Service worky-geo-locations
   APIWEATHERURL: 'http://api.weatherapi.com/v1/forecast.json', // Weather API Service URL
   APIWEATHERTOKEN: '062b9c8b637b4fc3875164009242005', // Weather API Service TOKEN
 };
