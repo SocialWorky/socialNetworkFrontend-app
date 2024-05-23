e4925ec4ebdd6ef1d7f4e2fa1b0604aa3269401b
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-23 14:51:54 -0400

    Update statistics component in admin module

--- src/app/modules/admin/shared/statistics/statistics.component.ts
+++ src/app/modules/admin/shared/statistics/statistics.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '@shared/interfaces/user.interface';
+import { UserService } from '@shared/services/users.service';
+import { PublicationService } from '@shared/services/publication.service';
 
 @Component({
   selector: 'worky-admin-statistics',
@@ -7,8 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent  implements OnInit {
 
-  constructor() { }
+  private subscription: Subscription = new Subscription();
 
-  ngOnInit() {}
+  private _allUsers: User[] = [];
+
+  private _countPublications: number = 0;
+
+  get allUsersLength() {
+    return this._allUsers.length;
+  }
+
+  get countPublications() {
+    return this._countPublications;
+  }
+
+  constructor (
+    private _userService: UserService,
+    private _publicationService: PublicationService,
+    private _cdr: ChangeDetectorRef,
+  ) { }
+
+  ngOnInit() {
+    this.getAllUsers();
+    this.getCountPublications();
+  }
+
+  private getAllUsers() {
+    this.subscription.add(this._userService.getAllUsers().subscribe((data) => {
+      this._allUsers = data;
+      this._cdr.markForCheck();      
+    }));
+  }
+
+  private getCountPublications() {
+    this.subscription.add(this._publicationService.getCountPublications().subscribe((data) => {
+      this._countPublications = data;
+      this._cdr.markForCheck();
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
