f3e12e1705d885577cf152e91b2a733db86cbe14
OSCARS26 <oscarolivare1@gmail.com>
2024-06-10 23:01:06 -0400

    refactor: Update profiles component to fix typo and improve code readability

--- src/app/modules/pages/profiles/profiles.component.ts
+++ src/app/modules/pages/profiles/profiles.component.ts
@@ -7,9 +7,10 @@ import { EditInfoProfileComponent } from '@shared/modules/edit-info-profile/edit
   templateUrl: './profiles.component.html',
   styleUrls: ['./profiles.component.scss'],
 })
-export class ProfilesComponent  implements OnInit {
+export class ProfilesComponent implements OnInit {
+  isCurrentUser: boolean = false;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) {}
 
   abrirFormulario() {
     const dialogRef = this.dialog.open(EditInfoProfileComponent, {
@@ -22,6 +23,25 @@ export class ProfilesComponent  implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  followUser() {
+    // Lógica para seguir al usuario
+    console.log('Following the user');
+  }
 
+  addFriend() {
+    // Lógica para añadir al usuario como amigo
+    console.log('Adding the user as a friend');
+  }
+
+  ngOnInit() {
+    this.checkIfCurrentUser();
+  }
+
+  checkIfCurrentUser() {
+    // Lógica para determinar si el usuario actual es el propietario del perfil
+    // Esto es un ejemplo y deberías reemplazarlo con tu lógica real
+    const currentUserId = 'user123'; // Supongamos que este es el ID del usuario actual
+    const profileOwnerId = 'user123'; // Supongamos que este es el ID del propietario del perfil
+    this.isCurrentUser = currentUserId === profileOwnerId;
+  }
 }
