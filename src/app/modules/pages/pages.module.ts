9f87e13004ba2c53cfa43606144ece1475713e80
Carlos Abarca Vega <soporte@compuelec.cl>
2024-06-18 20:46:28 -0400

    feat: Add cropperjs library for image editing in profiles component

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -19,7 +19,7 @@ import { AddPublicationModule } from '@shared/modules/addPublication/addPublicat
 import { WorkyWidgetModule } from '@shared/modules/worky-widget/worky-widget.module';
 import { PublicationViewModule } from '@shared/modules/publication-view/publication-view.module';
 import { SuggetionToFollowModule } from '@shared/modules/suggetion-to-follow/suggetion-to-follow.module';
-import { EditImgProfileComponent } from './profiles/components/edit-img/edit-img-profile.component';
+import { EditImgProfileComponent } from './profiles/components/edit-img-profile/edit-img-profile.component';
 import { EditInfoProfileComponent } from './profiles/components/edit-info-profile/edit-info-profile.component';
 import { ContactsModule } from '@shared/modules/contacts/contacts.module';
 import { WorkyButtonsModule } from '@shared/modules/buttons/buttons.module';
