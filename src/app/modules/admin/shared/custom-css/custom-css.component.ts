7fc2c3f897a551bde2d157a6c4f17a4116bc0b20
Carlos Abarca Vega <soporte@compuelec.cl>
2024-07-09 09:50:41 -0400

    refactor: Add lazy loading for custom CSS save button and show loader while saving

--- src/app/modules/admin/shared/custom-css/custom-css.component.ts
+++ src/app/modules/admin/shared/custom-css/custom-css.component.ts
@@ -1,22 +1,42 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { ConfigService } from '@shared/services/config.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'worky-custom-css',
   templateUrl: './custom-css.component.html',
   styleUrls: ['./custom-css.component.scss'],
 })
-export class CustomCssComponent  {
+export class CustomCssComponent implements OnDestroy {
 
   customCss: string = '';
+
+  customCssLoader: boolean = false;
+
+  private destroy$ = new Subject<void>();
 
   constructor( private _configService: ConfigService, private _cdr: ChangeDetectorRef ) {
     this.loadCustomCss();
   }
 
   async saveCustomCss() {
-    await this._configService.updateConfig({ customCss: this.customCss }).subscribe();
+    this.customCssLoader = true;
+    await this._configService.updateConfig({ customCss: this.customCss }).pipe(takeUntil(this.destroy$)).subscribe({
+      next: (data) => {
+        this.customCssLoader = false;
+        this._cdr.markForCheck();
+      },
+      error: () => {
+        this.customCssLoader = false;
+        this._cdr.markForCheck();
+      },
+    });
     this.applyCustomCss();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   async loadCustomCss() {
