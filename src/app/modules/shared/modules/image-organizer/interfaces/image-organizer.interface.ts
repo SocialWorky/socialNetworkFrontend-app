50c775961a91771a4b971ea13d632381a4b9a261
Carlos Abarca Vega <soporte@compuelec.cl>
2024-10-08 16:12:48 -0300

    refactor: Update image-organizer component to include comments and TypeView enum

--- src/app/modules/shared/modules/image-organizer/interfaces/image-organizer.interface.ts
+++ src/app/modules/shared/modules/image-organizer/interfaces/image-organizer.interface.ts
@@ -1,6 +1,14 @@
+import { Comment } from '@shared/interfaces/publicationView.interface';
+
 export interface ImageOrganizer {
   _id : string;
   url: string;
   urlThumbnail: string;
   urlCompressed: string;
+  comments: Comment[];
+}
+
+export enum TypeView {
+  PUBLICATION = 'publication',
+  COMMENT = 'comment'
 }
