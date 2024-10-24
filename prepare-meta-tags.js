c8f455d864d12d1b4a3a2897caac1014f1bcf58a
Carlos Abarca Vega <soporte@compuelec.cl>
2024-10-24 19:18:01 -0300

    Refactor package.json and package-lock.json to update dotenv dependency version

--- prepare-meta-tags.js
+++ prepare-meta-tags.js
@@ -1,36 +1,29 @@
+require('dotenv').config();
 const fs = require('fs');
-const path = require('path');
+const path = './src/index.html';
 
-const envVariables = {
-  title: process.env.META_TITLE,
-  description: process.env.META_DESCRIPTION,
-  ogSiteName: process.env.META_OG_SITE_NAME,
-  ogUrl: process.env.META_OG_URL,
-  ogTitle: process.env.META_OG_TITLE,
-  ogDescription: process.env.META_OG_DESCRIPTION,
-  ogImage: process.env.META_OG_IMAGE,
-  twitterCard: process.env.META_TWITTER_CARD,
-  twitterUrl: process.env.META_TWITTER_URL,
-  twitterTitle: process.env.META_TWITTER_TITLE,
-  twitterDescription: process.env.META_TWITTER_DESCRIPTION,
-  twitterImage: process.env.META_TWITTER_IMAGE
-};
+fs.readFile(path, 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
 
-const indexPath = path.join(__dirname, 'src', 'index.html');
-let indexContent = fs.readFileSync(indexPath, 'utf8');
+  let result = data.replace(/__TITLE__/g, process.env.NG_APP_META_TITLE)
+                   .replace(/__DESCRIPTION__/g, process.env.NG_APP_META_DESCRIPTION)
+                   .replace(/__KEYWORDS__/g, process.env.NG_APP_META_KEYWORDS)
+                   .replace(/_IMAGE_/g, process.env.NG_APP_META_IMAGE)
+                   .replace(/_URL_/g, process.env.NG_APP_META_URL)
+                   .replace(/__OG_SITE_NAME__/g, process.env.NG_APP_META_OG_SITE_NAME)
+                   .replace(/OG_URL/g, process.env.NG_APP_META_OG_URL)
+                   .replace(/__OG_TITLE__/g, process.env.NG_APP_META_OG_TITLE)
+                   .replace(/__OG_DESCRIPTION__/g, process.env.NG_APP_META_OG_DESCRIPTION)
+                   .replace(/OG_IMAGE/g, process.env.NG_APP_META_OG_IMAGE)
+                   .replace(/__TWITTER_CARD__/g, process.env.NG_APP_META_TWITTER_CARD)
+                   .replace(/TWITTER_URL/g, process.env.NG_APP_META_TWITTER_URL)
+                   .replace(/__TWITTER_TITLE__/g, process.env.NG_APP_META_TWITTER_TITLE)
+                   .replace(/__TWITTER_DESCRIPTION__/g, process.env.NG_APP_META_TWITTER_DESCRIPTION)
+                   .replace(/TWITTER_IMAGE/g, process.env.NG_APP_META_TWITTER_IMAGE);
 
-indexContent = indexContent
-  .replace(/content="" name="title"/, `content="${envVariables.title}" name="title"`)
-  .replace(/content="" name="description"/, `content="${envVariables.description}" name="description"`)
-  .replace(/content="" property="og:site_name"/, `content="${envVariables.ogSiteName}" property="og:site_name"`)
-  .replace(/content="" property="og:url"/, `content="${envVariables.ogUrl}" property="og:url"`)
-  .replace(/content="" property="og:title"/, `content="${envVariables.ogTitle}" property="og:title"`)
-  .replace(/content="" property="og:description"/, `content="${envVariables.ogDescription}" property="og:description"`)
-  .replace(/content="" property="og:image"/, `content="${envVariables.ogImage}" property="og:image"`)
-  .replace(/content="" property="twitter:card"/, `content="${envVariables.twitterCard}" property="twitter:card"`)
-  .replace(/content="" property="twitter:url"/, `content="${envVariables.twitterUrl}" property="twitter:url"`)
-  .replace(/content="" property="twitter:title"/, `content="${envVariables.twitterTitle}" property="twitter:title"`)
-  .replace(/content="" property="twitter:description"/, `content="${envVariables.twitterDescription}" property="twitter:description"`)
-  .replace(/content="" property="twitter:image"/, `content="${envVariables.twitterImage}" property="twitter:image"`);
-
-fs.writeFileSync(indexPath, indexContent, 'utf8');
+  fs.writeFile(path, result, 'utf8', (err) => {
+     if (err) return console.log(err);
+  });
+});
