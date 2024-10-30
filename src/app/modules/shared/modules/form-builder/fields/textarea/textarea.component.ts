cf6f19c7cad70e0c009e5d02951bdcdcefd1bdcb
Carlos Abarca Vega <soporte@compuelec.cl>
2024-10-30 14:54:23 -0300

    Add TextareaComponent with template and ControlValueAccessor implementation

---
+++ src/app/modules/shared/modules/form-builder/fields/textarea/textarea.component.ts
@@ -0,0 +1,48 @@
+// src/app/form-builder/fields/textarea.component.ts
+import { Component, Input, SimpleChanges, forwardRef, OnChanges } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormControl } from '@angular/forms';
+import { Field } from '../../interfaces/field.interface';
+
+@Component({
+  selector: 'app-textarea',
+  templateUrl: './textarea.component.html',
+  styleUrls: ['./textarea.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaComponent), // forwardRef es crucial aquí
+      multi: true
+    }
+  ]
+})
+export class TextareaComponent implements ControlValueAccessor, OnChanges {
+  @Input() field!: Field;
+  control = new FormControl('');
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['field']) {
+      this.control.setValue(this.field.value || '');
+    }
+  }
+
+  // Implementación de ControlValueAccessor
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  writeValue(value: string): void {
+    this.control.setValue(value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+    this.control.valueChanges.subscribe(fn); // Escucha los cambios en el control
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.control.disable() : this.control.enable();
+  }
+}
