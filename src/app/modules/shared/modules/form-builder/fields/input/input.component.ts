52b09e662be2cf256e4a42224aa0e94420e52690
Carlos Abarca Vega <soporte@compuelec.cl>
2024-10-30 14:53:37 -0300

    Add InputComponent with template and control value accessor implementation

---
+++ src/app/modules/shared/modules/form-builder/fields/input/input.component.ts
@@ -0,0 +1,46 @@
+// input.component.ts
+import { Component, Input, SimpleChanges, OnChanges, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormControl } from '@angular/forms';
+import { Field } from '../../interfaces/field.interface';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent), // Usar forwardRef
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements ControlValueAccessor, OnChanges {
+  @Input() field!: Field;
+  control = new FormControl('');
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['field']) {
+      this.control.setValue(this.field.value || '');
+    }
+  }
+
+  writeValue(value: string): void {
+    this.control.setValue(value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+    this.control.valueChanges.subscribe(fn); // Suscribirse a cambios
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.control.disable() : this.control.enable();
+  }
+}
