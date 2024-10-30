48a8326468c43475558a25bdcc27ef6449b0c0a8
Carlos Abarca Vega <soporte@compuelec.cl>
2024-10-30 14:54:06 -0300

    Add SelectComponent with template and ControlValueAccessor implementation

---
+++ src/app/modules/shared/modules/form-builder/fields/select/select.component.ts
@@ -0,0 +1,49 @@
+// src/app/form-builder/fields/select.component.ts
+import { Component, Input, OnChanges, SimpleChanges, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormControl } from '@angular/forms';
+import { Field } from '../../interfaces/field.interface';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ]
+})
+export class SelectComponent implements ControlValueAccessor, OnChanges {
+  @Input() field!: Field;
+  control = new FormControl('');
+
+  // Implementación de ControlValueAccessor
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['field']) {
+      this.control.setValue(this.field.value || '');
+    }
+  }
+
+  writeValue(value: string): void {
+    this.control.setValue(value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+    this.control.valueChanges.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.control.disable() : this.control.enable();
+  }
+}
