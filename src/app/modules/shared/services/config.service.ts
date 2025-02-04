5e728d87c6a73a6d2f191546b0096d95727dc26b
Carlos Abarca Vega <soporte@compuelec.cl>
2025-02-04 19:58:14 -0300

    Remove token handling from ConfigService and simplify HTTP requests

--- src/app/modules/shared/services/config.service.ts
+++ src/app/modules/shared/services/config.service.ts
@@ -13,8 +13,6 @@ import { Router } from '@angular/router';
 export class ConfigService {
   private apiUrl: string;
 
-  private token = localStorage.getItem('token');
-
   private configSubject = new BehaviorSubject<any>(null);
 
   private _unsubscribeAll = new Subject<void>();
@@ -26,36 +24,23 @@ export class ConfigService {
     private socket: Socket,
     private _router: Router
   ) {
-    this.token = localStorage.getItem('token');
-    if (!this.token) this._router.navigate(['/']);
     this.subscribeToNotification();
     this.apiUrl = environment.API_URL;
   }
 
-  private getHeaders(): HttpHeaders {
-    const token = this.token;
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-    return headers;
-  }
-
   getConfig(): Observable<any> {
     const url = `${this.apiUrl}/config`;
-    const headers = this.getHeaders();
-    return this.http.get<any>(url, { headers });
+    return this.http.get<any>(url);
   }
 
   getConfigServices(): Observable<ConfigServiceInterface> {
     const url = `${this.apiUrl}/config/services`;
-    const headers = this.getHeaders();
-    return this.http.get<ConfigServiceInterface>(url, { headers });
+    return this.http.get<ConfigServiceInterface>(url);
   }
 
   updateConfig(config: Config): Observable<Config> {
     const url = `${this.apiUrl}/config`;
-    const headers = this.getHeaders();
-    return this.http.put<Config>(url, config, { headers }).pipe(
+    return this.http.put<Config>(url, config).pipe(
       tap((updatedConfig) => {
         this.socket.emit('updateConfig', updatedConfig);
         this.configSubject.next(updatedConfig);
